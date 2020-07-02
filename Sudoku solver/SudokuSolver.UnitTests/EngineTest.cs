17764b175bbcc7712a37ff6975452fe3a9ae6a29
harkor15 <kibkib1998@gmail.com>
2020-07-03 00:50:51 +0200

    New unit tests

---
+++ Sudoku solver/SudokuSolver.UnitTests/EngineTest.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sudoku_solver;
+
+namespace SudokuSolver.UnitTests
+{
+    [TestClass]
+    public class EngineTests
+    {
+        [TestMethod]
+        public void ValidTest()
+        {
+            var ds = new DataSet();
+            int[,] sudoku ={
+                { 3, 2, 1, 7, 0, 4, 0, 0, 0 },
+                { 6, 4, 0, 0, 9, 0, 0, 0, 7 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 4, 5, 9, 0, 0 },
+                { 0, 0, 5, 1, 8, 7, 4, 0, 0 },
+                { 0, 0, 4, 9, 6, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 2, 0, 0, 0, 7, 0, 0, 1, 9 },
+                { 0, 0, 0, 6, 0, 9, 5, 8, 2 }
+                };
+            ds.sudokuData = sudoku;
+            var result = Engine.IsDataValid(ds);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void ValidTest2()
+        {
+            var ds = new DataSet();
+            int[,] sudoku ={
+                { 3, 2, 1, 7, 0, 4, 0, 0, 0 },
+                { 6, 4, 0, 0, 9, 0, 0, 0, 7 },
+                { 0, 0, 0, 7, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 4, 5, 9, 0, 0 },
+                { 0, 0, 5, 1, 8, 7, 4, 0, 0 },
+                { 0, 0, 4, 9, 6, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 2, 0, 0, 0, 7, 0, 0, 1, 9 },
+                { 0, 0, 0, 6, 0, 9, 5, 8, 2 }
+                };
+            ds.sudokuData = sudoku;
+            var result = Engine.IsDataValid(ds);
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void ValidTest3()
+        {
+            var ds = new DataSet();
+            int[,] sudoku ={
+                { 3, 2, 1, 7, 0, 4, 0, 0, 0 },
+                { 6, 4, 0, 0, 9, 0, 0, 0, 7 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 4, 5, 9, 0, 0 },
+                { 0, 0, 5, 1, 8, 7, 4, 0, 0 },
+                { 0, 0, 4, 9, 6, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 2, 0, 0, 0, 7, 0, 0, 1, 9 },
+                { 0, 0, 0, 6, 0, 9, 5, 8, 1 }
+                };
+            ds.sudokuData = sudoku;
+            var result = Engine.IsDataValid(ds);
+            Assert.IsFalse(result);
+        }
+        [TestMethod]
+        public void SolvingTest()
+        {
+            var ds = new DataSet();
+            int[,] sudoku ={
+                { 3, 2, 1, 7, 0, 4, 0, 0, 0 },
+                { 6, 4, 0, 0, 9, 0, 0, 0, 7 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 4, 5, 9, 0, 0 },
+                { 0, 0, 5, 1, 8, 7, 4, 0, 0 },
+                { 0, 0, 4, 9, 6, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 2, 0, 0, 0, 7, 0, 0, 1, 9 },
+                { 0, 0, 0, 6, 0, 9, 5, 8, 2 }
+                };
+            ds.sudokuData = sudoku;
+            var result=Engine.ComputeResult(ds);
+           
+            int[,] expected ={
+                { 3,2,1,7,5,4,6,9,8 },
+                { 6,4,8,2,9,3,1,5,7 },
+                { 5,7,9,8,1,6,2,3,4 },
+                { 7,8,2,3,4,5,9,6,1 },
+                { 9,6,5,1,8,7,4,2,3 },
+                { 1,3,4,9,6,2,8,7,5 },
+                { 8,9,3,5,2,1,7,4,6 },
+                { 2,5,6,4,7,8,3,1,9 },
+                { 4,1,7,6,3,9,5,8,2 }
+                };
+
+            for(int i=0; i < 9; i++)
+            {
+                for( int j = 0; j < 9; j++)
+                {
+                    Assert.AreEqual(expected[i, j], result.sudokuData[i, j]);
+
+                }
+            }
+
+               
+            
+        }
+
+    }
+}
---
+++ Sudoku solver/SudokuSolver.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SudokuSolver.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SudokuSolver.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("561b403a-3fea-4748-996e-519ea23dbbee")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
